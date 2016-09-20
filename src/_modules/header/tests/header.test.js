e6e93e69c03089d3591310842465c30172004772
Jonas Tebbe <jonastebbe@googlemail.com>
2016-09-20 23:14:26 +0200

    Added photo subpage

---
+++ src/_modules/header/tests/header.test.js
@@ -0,0 +1,16 @@
+/*eslint no-unused-expressions:0 */
+'use strict';
+
+import Header from '../header';
+
+describe('Header View', function() {
+
+  beforeEach(() => {
+    this.header = new Header();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.header).to.exist;
+  });
+
+});
