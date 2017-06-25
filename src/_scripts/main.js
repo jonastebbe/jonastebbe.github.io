d6b56b45d6c60ca4006e540a26a8b1986fcfa46b
Jonas Tebbe <jonastebbe@googlemail.com>
2017-06-25 22:45:44 +0200

    Adds barba.js for page transitions

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,10 +6,51 @@
 import $ from 'jquery';
 import Link from '../_modules/link/link';
 import Blazy from 'blazy';
+import Barba from 'barba.js';
 
 $(() => {
-  new Link(); // Activate Link modules logic
+  // Activate Link modules logic
+  new Link();
+
+  // Lazy image loading
   var bLazy = new Blazy({
   });
   bLazy.revalidate();
+
+  // Barba custom transition
+  var FadeTransition = Barba.BaseTransition.extend({
+    start: function() {
+      Promise
+        .all([this.newContainerLoading, this.fadeOut()])
+        .then(this.fadeIn.bind(this));
+    },
+
+    fadeOut: function() {
+      return $(this.oldContainer).animate({ opacity: 0 }).promise();
+    },
+
+    fadeIn: function() {
+      var _this = this;
+      var $el = $(this.newContainer);
+
+      $(this.oldContainer).hide();
+
+      $el.css({
+        visibility : 'visible',
+        opacity : 0
+      });
+
+      $el.animate({ opacity: 1 }, 400, function() {
+        _this.done();
+        bLazy.revalidate();
+      });
+    }
+  });
+
+  Barba.Pjax.getTransition = function() {
+    return FadeTransition;
+  };
+
+  // AJAX page transitions
+  Barba.Pjax.start();
 });
